24839b458d4702b7142667483d7a42e57c8f1ee7
Hana-latif98 <hana.latif@universalavenue.com>
2020-01-09 09:05:08 +0100

    one-js-for-municipilaty-drop-down

--- script/municipilaty-drop-down.js
+++ script/municipilaty-drop-down.js
@@ -12,3 +12,20 @@ window.onload = function offClick(){
         }
     }
 } /*open and close kommun dropdown*/
+
+function toggleActive(id) {
+    document.getElementById(id).classList.toggle("dropdown-content-active");
+    const name = document.getElementById(id).innerHTML;
+    if (document.getElementById('dropDownButton').innerHTML.includes(name)) {
+        removedChoice = document.getElementById('dropDownButton').innerHTML.replace(name + ', ', "");
+        document.getElementById('dropDownButton').innerHTML = removedChoice;
+    } else {
+        removedAlla = document.getElementById('dropDownButton').innerHTML.replace('Alla...', '');
+        document.getElementById('dropDownButton').innerHTML = removedAlla;
+        document.getElementById('dropDownButton').innerHTML += name + ', ';
+    }
+    if (document.getElementById('dropDownButton').innerHTML === '') {
+        const text = ('Alla...');
+        document.getElementById('dropDownButton').innerHTML = text;
+    }
+} /*replace text information for municipilaty drop down*/
